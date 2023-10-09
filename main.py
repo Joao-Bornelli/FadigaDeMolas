1159908d3ea59345540838ec73a628e01d35acd1
JoaoBornelli <joaoluisbornelli@gmail.com>
2023-10-09 12:47:56 -0300

    changed the window to open in fullscreen

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import tkinter as tk
 from build import gui
 
 root = tk.Tk()
-
+root.attributes('-fullscreen', True)
 # Create an instance of the generated GUI class
+
+def exit_fullscreen(event):
+    root.attributes('-fullscreen', False)
+
+root.bind('<Escape>', exit_fullscreen)
+
 app = gui(root)
+
+
 
 # Start the GUI event loop
 root.mainloop()
